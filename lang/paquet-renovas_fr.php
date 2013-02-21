aa84473333fe3e7fc2da8048b4e6edfc05027ae4
abelass <websolutions@mychacra.net>
2013-02-21 09:52:29 +0100

    import initial

---
+++ lang/paquet-renovas_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'renovas_description' => 'Installe les plugins et contient quelques configuration de base',
+	'renovas_nom' => 'Renovas',
+	'renovas_slogan' => 'La Base',
+);
+
+?>
---
+++ renovas_pipelines.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Utilisations de pipelines par Renovas
+ *
+ * @plugin     Renovas
+ * @copyright  2013
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Renovas\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+?>
