0c9df042565a283c0bbe1f14897b6df6fe93ec26
Rainer <rainer@websimple.be>
2019-03-06 13:43:30 +0100

    eviter des erreurs, retour par défaut id_rubrique du cintexte

--- renovas_fonctions.php
+++ renovas_fonctions.php
@@ -23,12 +23,16 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  *   L'identifiant du secteur correpondnanbt
  */
 function get_id_source($id_rubrique, $ids_secteur) {
-	if (!$id_source = sql_getfetsel(
+	if (!$id_rubrique = sql_getfetsel(
 		'id_rubrique', 
 		'spip_rubriques',
 		'id_rubrique=' . $id_rubrique . ' AND id_rubrique IN (' .implode(',',$ids_secteur) . ')')) {
-		$id_parent = sql_getfetsel('id_parent', 'spip_rubriques', 'id_rubrique=' . $id_rubrique);
-		$id_source = get_id_source($id_parent, $ids_secteur);
+		if ($id_rubrique AND $id_parent = sql_getfetsel(
+			'id_parent', 
+			'spip_rubriques', 
+			'id_rubrique=' . $id_rubrique)) {
+			$id_rubrique = get_id_source($id_parent, $ids_secteur);
+		}
 	}
-	return $id_source;
+	return $id_rubrique;
 }
